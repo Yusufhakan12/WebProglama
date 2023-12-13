9e1ae982b3094993890bcf3d6d1fdfa8b6365bfb
yusuf hakan güneş <94203440+Yusufhakan12@users.noreply.github.com>
2023-12-13 20:15:00 +0300

    WebProglama

---
+++ WebApplication15/Models/LoginPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication15.Models
+{
+#nullable disable
+    public class LoginPageViewModel
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
