16a6ee4104d0deb548c949d4b2cfcbf4ceb98751
yusuf hakan güneş <94203440+Yusufhakan12@users.noreply.github.com>
2023-12-19 19:29:25 +0300

    Wb

---
+++ WebApplication15/Controllers/UcakController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication15.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApplication15.Data;
+
+namespace WebApplication15.Controllers
+{
+
+  
+    public class UcakController : Controller
+    {
+        private readonly AppDbUcakContext _appDbUcakContext;
+
+        public UcakController(AppDbUcakContext appDbUcakContext)
+        {
+            _appDbUcakContext = appDbUcakContext;
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> UcakEkle()
+        {
+            var Flys = await _appDbUcakContext.FlyNames.ToListAsync();
+            return View(Flys);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UcakEkle(UcakEkleViewModel model)
+        {
+            var FlyName = new FlyName
+            {
+                Id = model.Id.GetHashCode(),
+                Name = model.Name,
+
+            };
+            await _appDbUcakContext.FlyNames.AddAsync(FlyName);
+            await _appDbUcakContext.SaveChangesAsync();
+
+            return RedirectToAction("UcakEkle", "Ucak");
+        }
+    }
+}
