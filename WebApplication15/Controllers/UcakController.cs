b7e21fa9e3d52043f9b7e63eaf0eb2d599013cc8
yusuf hakan güneş <94203440+Yusufhakan12@users.noreply.github.com>
2023-12-21 23:32:01 +0300

    webProglama
    SeferEKleme

--- WebApplication15/Controllers/UcakController.cs
+++ WebApplication15/Controllers/UcakController.cs
@@ -19,22 +19,80 @@ namespace WebApplication15.Controllers
 
         public async Task<IActionResult> UcakEkle()
         {
-            var Flys = await _appDbUcakContext.FlyNames.ToListAsync();
-            return View(Flys);
+            //var Flys = await _appDbUcakContext.FlyNames.ToListAsync();
+            return View();
         }
         [HttpPost]
-        public async Task<IActionResult> UcakEkle(UcakEkleViewModel model)
+        public async Task<IActionResult> UcakEkle(SeferEkleViewModel model)
         {
-            var FlyName = new FlyName
+            var Voyage = new Voyage
             {
-                Id = model.Id.GetHashCode(),
-                Name = model.Name,
+                VoyageId = model.VoyageId.GetHashCode(),
+
+                From = model.From,
+                To = model.To,
+                AirPlaneName = model.AirPlaneName,
+                FromDate = model.FromDate.ToUniversalTime(),
+                capacity = model.capacity,
+
+
+
 
             };
-            await _appDbUcakContext.FlyNames.AddAsync(FlyName);
+           
+            await _appDbUcakContext.Voyages.AddAsync(Voyage);
             await _appDbUcakContext.SaveChangesAsync();
 
             return RedirectToAction("UcakEkle", "Ucak");
+        }
+
+        public async Task<IActionResult> SeferDuzenle(SeferEkleViewModel model)
+        {
+            var Flys = await _appDbUcakContext.Voyages.ToListAsync();
+            return View(Flys);
+        }
+        [HttpGet]
+        public async Task<IActionResult> SeferGuncelle(int id)
+        {
+            var Flys = await _appDbUcakContext.Voyages.FirstOrDefaultAsync(x => x.VoyageId == id);
+
+            if (Flys != null)
+            {
+                var ViewModel = new SeferUpdateViewModel()
+                {
+                  VoyageId=Flys.VoyageId,
+                  From = Flys.From,
+                  FromDate = Flys.FromDate,
+                  To = Flys.To,
+                  AirPlaneName = Flys.AirPlaneName,
+                  capacity=Flys.capacity
+                };
+
+                return await Task.Run(() => View("SeferGuncelle", ViewModel));
+            }
+
+            return RedirectToAction("SeferDuzenle", "Ucak");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SeferGuncelle(SeferUpdateViewModel model)
+        {
+            var Flys = await _appDbUcakContext.Voyages.FindAsync(model.VoyageId);
+            if (Flys != null)
+            {
+                Flys.capacity = model.capacity;
+                Flys.FromDate=model.FromDate.ToUniversalTime();
+                Flys.To = model.To;
+                Flys.AirPlaneName = Flys.AirPlaneName;
+                Flys.From=model.From;
+
+
+                await _appDbUcakContext.SaveChangesAsync();
+
+                return RedirectToAction("SeferDuzenle", "Ucak");
+
+            }
+            return RedirectToAction("SeferDuzenle", "Ucak");
         }
     }
 }
--- WebApplication15/Migrations/AppDbUcak/20231220163828_SeferBilgileri.Designer.cs
+++ WebApplication15/Migrations/AppDbUcak/20231220163828_SeferBilgileri.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApplication15.Data;
@@ -11,9 +12,10 @@ using WebApplication15.Data;
 namespace WebApplication15.Migrations.AppDbUcak
 {
     [DbContext(typeof(AppDbUcakContext))]
-    partial class AppDbUcakContextModelSnapshot : ModelSnapshot
+    [Migration("20231220163828_SeferBilgileri")]
+    partial class SeferBilgileri
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
